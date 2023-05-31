18bcbc7e4c4f7628ddaecb731c9918218ca42cd1
LucasChaosMind <lucascaladob@gmail.com>
2023-05-30 21:35:17 -0300

    New page and new components

---
+++ components/footer.js
@@ -0,0 +1,52 @@
+const footer=`
+    <div class='footer container-fluid d-flex p-2 w-100'>
+    <div class='left fw500 d-flex flex-column p-1'>
+        <h1 class='logo'>LifeCycles</h1>
+        <p class='desc pt-1 pe-1'>For all the cycles of your life, from momentary to continuous cycles, the waycycles will be with you</p>
+        <div class='social d-flex gap-2'>
+            <div class='social-icon text-white d-flex justify-content-center align-items-center' id="icon-facebook">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+            </div>
+            <div class='social-icon text-white d-flex justify-content-center align-items-center' id="icon-instagram" >
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class='social-icon text-white d-flex justify-content-center align-items-center' id="icon-twitter">
+                <a href='#'><i class="fab fa-twitter"></i></a>
+            </div>
+            <div class='social-icon text-white d-flex justify-content-center align-items-center' id="icon-pinterest">
+                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+            </div>
+        </div>
+    </div>
+    <div className='center p-2'>
+        <h3 class='title fw500 pb-2'>Usefull Links</h3>
+        <ul class='list d-flex flex-wrap'>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>Home</a></li>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>Cart</a></li>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>My Account</a></li>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>Who we are?</a></li>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>Support</a></li>
+            <li class='list-item w-50 p-0 mb-1'><a href='#' class='link'>Terms</a></li>
+        </ul>
+    </div>
+    <div className='right p-1'>
+        <h3 class='title fw500 pb-2'>Contact</h3>
+        <div class='contact-item mb-3 d-flex align-items-center g-1 pointer-event'>
+            <i class="fas fa-home"></i>
+            <label class='ms-2'>Put the local here</label>
+        </div>
+        <div className='contact-item mb-3 d-flex align-items-center g-1'>
+            <i class="fas fa-phone"></i>
+            <label class='ms-2'>+1 234 567 890</label>
+        </div>
+        <div className='contact-item mb-3 d-flex align-items-center g-1'>
+            <i class="fas fa-at"></i>
+            <label class='ms-2'>YourEmailHere@LifeCycles.com</label>
+        </div>
+    </div>
+    </div>`;
+const head =`
+        <link rel="stylesheet" href="../components/css/footer.css">
+`;
+document.querySelector('#footer').innerHTML += footer; 
+document.querySelector('head').innerHTML += head; 
---
+++ components/header.js
@@ -0,0 +1,40 @@
+const header = `
+<nav class="navbar navbar-expand-lg  navbar-light bg-white py-4 fixed-top order-lg-0">
+<div class="container">
+    <a class="navbar-brand d-flex justify-content-between align-items-center" href="home.html">
+        <img src="../img/logo.jpeg">
+        <span class="fw-lighter ms-2">Life Cycle</span>
+    </a>
+    <div class="links order-lg-2 nav-btns">
+        <a class="btn position-relative me-4">
+            <i class="fa fa-shopping-cart"></i>
+            <span class="position-absolute top-0 start-100 translate-middle badge bg-primary">2</span>
+        </a>
+        <a class="btn position-relative me-4">
+            <i class="fa fa-user"></i>
+            <span class="position-absolute top-0 start-100 translate-middle badge bg-primary">?</span>
+        </a>
+        <a class="btn position-relative">
+            <i class="fa fa-search"></i>
+        </a>
+    </div>
+    <a class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+        <span class="navbar-toggler-icon"></span>
+    </a>
+
+    <div class="collapse navbar-collapse order-lg-1" id="navMenu">
+        <ul class="navbar-nav mx-auto text-center d-flex justify-content-between align-items-center">
+            <li class="nav-item px-2 py-2"><a class="nav-link text-dark" href="#">Home</a></li>
+            <li class="nav-item px-2 py-2"><a class="nav-link text-dark" href="#">Collection</a></li>
+            <li class="nav-item px-2 py-2"><a class="nav-link text-dark" href="#">Specials</a></li>
+            <li class="nav-item px-2 py-2"><a class="nav-link text-dark" href="#">Popular</a></li>
+        </ul>
+    </div>
+</div>
+</nav>
+`
+const head =`
+        <link rel="stylesheet" href="../components/css/header.css">
+`;
+document.querySelector('header').innerHTML += footer; 
+document.querySelector('head').innerHTML += head; 
